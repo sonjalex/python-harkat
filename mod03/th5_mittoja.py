9293f24b2d439e66cf39c9585099e3f7c703af55
Sonja Okkola <okkola.sonja@gmail.com>
2026-08-25 09:24:11 +0300

    muokkauksia

--- mod03/th5_mittoja.py
+++ mod03/th5_mittoja.py
@@ -26,4 +26,4 @@ grammat_lasku = ((luodit + luodit_lasku) * 13.3)
 kilogramma = (grammat_lasku // 1000)
 jakojäännös = (grammat_lasku % 1000)
 
-print(f"{kilogramma} kilogrammaa ja {jakojäännös} grammaa")
+print(f"{kilogramma: .0f} kilogrammaa ja {jakojäännös: .2f} grammaa")
--- mod03/th6_arvottu.py
+++ mod03/th6_arvottu.py
@@ -3,3 +3,16 @@ import random
 print(random.sample(range(0, 9), 3))
 ##Nelinumeroinen koodi, jonka kukin numeromerkki on väliltä 1-6
 print(random.sample(range(1, 6), 4))
+
+##Tai random.radintin kanssa
+
+luku01 = int(random.randint(0,9))
+luku02 = int(random.randint(0,9))
+luku03 = int(random.randint(0,9))
+print(luku01, luku02, luku03)
+
+luku1 = int(random.randint(1,6))
+luku2 = int(random.randint(1,6))
+luku3 = int(random.randint(1,6))
+luku4 = int(random.randint(1,6))
+print(luku1, luku2, luku3, luku4)
